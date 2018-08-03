5be5a62aa9a65465a92447331ae1ddd1abd4202d
ekinakyurek <akyurekekin@gmail.com>
2018-08-03 15:38:34 -0400

    imdb and macnet added

---
+++ macnet/demosetup.jl
@@ -0,0 +1,7 @@
+server="ai.ku.edu.tr/"
+info("Downloading sample questions and images from CLEVR dataset...")
+download(server*"data/mac-network/demo.tar.gz","demo.tar.gz")
+run(`tar -xzf demo.tar.gz`)
+rm("demo.tar.gz")
+info("Downloading pre-trained model from our servers...")
+download(server*"models/mac-network/demo_model.jld","models/macnet.jld")
