047fd0547e9fe338d927d3cfacb457c7b537b6b0
ekinakyurek <akyurekekin@gmail.com>
2018-10-04 01:05:27 -0400

    v1.0 compat

--- macnet/demosetup.jl
+++ macnet/demosetup.jl
@@ -1,21 +1,13 @@
-if ENV["HOME"] == "/mnt/juliabox"
-    Pkg.dir(path...)=joinpath("/home/jrun/.julia/v0.6",path...)
-else
-    for p in ("Knet","JLD","JSON","Images") # ,"WordTokenizers")
-        Pkg.installed(p) == nothing && Pkg.add(p)
-    end
-end
-using Images,JLD,Knet # ,WordTokenizers
-global atype = gpu()<0 ? Array{Float32}:KnetArray{Float32}
-
-server="people.csail.mit.edu/deniz/"
+server="ai.ku.edu.tr/"
 if !isdir("data/demo")
-    info("Downloading sample questions and images from CLEVR dataset...")
+    println("Downloading sample questions and images from CLEVR dataset...")
     download(server*"data/mac-network/demo.tar.gz","demo.tar.gz")
     run(`tar -xzf demo.tar.gz`)
     rm("demo.tar.gz")
 end
-if !isfile("models/macnet.jld")
-    info("Downloading pre-trained model from our servers...")
-    download(server*"models/mac-network/demo_model.jld","models/macnet.jld")
+
+if !isfile("models/macnet.jld2")
+    println("Downloading pre-trained model from our servers...")
+    download(server*"models/mac-network/macnet.jld2","models/macnet.jld2")
 end
+println("Demo setup is completed")
